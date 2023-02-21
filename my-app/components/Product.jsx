f3656b0f58c6db6a69a0504916d71958560b2262
kevinmbuicode <kevinmushan@gmail.com>
2023-02-21 23:43:16 +0300

    feat: modify product

--- my-app/components/Product.jsx
+++ my-app/components/Product.jsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { urlFor } from '../lib/client';
 
 const Product = ({ product: { image, name, slug, price } }) => {
+ 
   return (
     <div>
       <Link href={`/product/${slug.current}`}>
@@ -15,7 +16,7 @@ const Product = ({ product: { image, name, slug, price } }) => {
             className="product-image"
           />
           <p className="product-name">{name}</p>
-          <p className="product-price">Ksh {price}</p>
+          <p className="product-price">${price}</p>
         </div>
       </Link>
     </div>
