5f450e9d07bb0afd7e72be1080819c495f402bd2
kevinmbuicode <kevinmushan@gmail.com>
2023-02-21 23:45:44 +0300

    feat: verify products at index

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -22,10 +22,10 @@ const Home = ({ products, bannerData }) => (
 export const getServerSideProps = async () => {
   const query = '*[_type == "product"]';
   const products = await client.fetch(query);
-
+  
   const bannerQuery = '*[_type == "banner"]';
   const bannerData = await client.fetch(bannerQuery);
-
+  
   return {
     props: { products, bannerData }
   }
